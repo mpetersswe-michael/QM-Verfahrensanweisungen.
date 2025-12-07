f5817b11829f3af8cc4bbc705e3699a46fa042d4
mpetersswe-michael <m.petersswe@gmail.com>
2025-12-07 10:33:00 +0100

    Update app.py

--- app.py
+++ app.py
@@ -213,7 +213,7 @@ with tab1:
             )
             st.dataframe(df_filtered, use_container_width=True)
 
-                      csv_data = df_all.to_csv(index=False, sep=";", encoding="utf-8-sig").encode("utf-8-sig")
+            sv_data = df_all.to_csv(index=False, sep=";", encoding="utf-8-sig").encode("utf-8-sig")
             st.download_button(
                 label="VA-Tabelle als CSV herunterladen",
                 data=csv_data,
