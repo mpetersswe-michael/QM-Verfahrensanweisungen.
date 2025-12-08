99d02b846aae2d59780cb252c9f27910ae22949c
mpetersswe-michael <m.petersswe@gmail.com>
2025-12-08 11:17:10 +0100

    Update app.py

--- app.py
+++ app.py
@@ -320,7 +320,7 @@ with tabs[1]:
         va_liste = sorted(df_va["VA_Nr"].dropna().unique())
         va_zum_loeschen = st.selectbox("VA auswählen zum Löschen", options=va_liste, index=None, key="va_loeschen_select")
 
-          if va_zum_loeschen and st.button("VA löschen", key="va_loeschen_button"):
+             if va_zum_loeschen and st.button("VA löschen", key="va_loeschen_button"):
         df_va = df_va[df_va["VA_Nr"] != va_zum_loeschen]
         df_va.to_csv(DATA_FILE_QM, sep=";", index=False, encoding="utf-8-sig")
         st.success(f"❌ VA {va_zum_loeschen} wurde gelöscht.")
